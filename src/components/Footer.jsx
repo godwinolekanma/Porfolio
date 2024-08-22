93678e92c328d14ba19a0774161f9b471f4d1c6f
godwinolekanma <89326756+godwinolekanma@users.noreply.github.com>
2024-08-22 09:07:36 -0500

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,9 +10,9 @@ function Footer (){
     <h3>Developed by Godwin Olekanma</h3>
     <h3>Copyright © {year}</h3>
         <ul className="socials">
-            <li><a href=""><i class='bx bxl-github'></i></a></li>
-            <li><a><i class='bx bxl-twitter' ></i></a></li>
-            <li><a><i class='bx bxl-facebook-circle' ></i></a></li>
+            <li><a href="https://www.linkedin.com/in/godwin-olekanma-157072192"><i class='bx bxl-github'></i></a></li>
+            <li><a href="https://x.com/golekanma"><i class='bx bxl-twitter' ></i></a></li>
+            <li><a href="https://m.facebook.com/godwin.olekanma/"><i class='bx bxl-facebook-circle' ></i></a></li>
             <li><a href="mailto:godwinolekanma12@gmail.com"><i class='bx bxl-gmail' ></i></a></li>
         </ul>
     </div>
